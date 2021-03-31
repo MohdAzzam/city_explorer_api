e9444bd749a63ce47e69d984c7868f710c83ecb3
MohdAzzam <alazzamhmode@gmail.com>
2021-03-31 13:42:01 +0300

    fixing the schema issue

--- schema.sql
+++ schema.sql
@@ -1,4 +1,8 @@
-CREATE  TABLE IF NOT EXISTS location (name VARCHAR(255),display_name VARCHAR(255),latitude VARCHAR(100) ,longitude VARCHAR(100));
+drop table if exists location;
+drop table if exists weather;
+drop table if exists Park;
+
+CREATE  TABLE IF NOT EXISTS location (search_query VARCHAR(255),display_name VARCHAR(255),latitude VARCHAR(100) ,longitude VARCHAR(100));
 
 CREATE  TABLE IF NOT EXISTS weather (time VARCHAR(155),forecast VARCHAR (255));
 
